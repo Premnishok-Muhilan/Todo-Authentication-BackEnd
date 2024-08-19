98ae2e6e34145885a4d0ff07d2e3c532998e771d
Premnishok-Muhilan <premnishok4@gmail.com>
2024-08-19 09:14:13 +0530

    Moved routes to todoRoutes.js and imported the todoRouter in app.js

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ app.get("/", (request, response) => {
   response.send("Hello using experss JS");
 });
 
+//import todoRouter
+const todoRouter = require("./routes/todoRoutes");
+//To use router from todoRoutes.js
+app.use("/api/v1/todos", todoRouter);
+
 //sample post request which sends the request body as response
 // app.post("/api/v1/todos", async (req, res) => {
 //   try {
@@ -28,24 +33,25 @@ app.get("/", (request, response) => {
 //   }
 // });
 
-app.post("/api/v1/todos", async (req, res) => {
-  try {
-    // get the description from the request body
-    const { description } = req.body;
+//Moved to todoRoutes.js
+// app.post("/api/v1/todos", async (req, res) => {
+//   try {
+//     // get the description from the request body
+//     const { description } = req.body;
 
-    // create a new todo
-    const newTodo = new Todo({
-      description,
-    });
+//     // create a new todo
+//     const newTodo = new Todo({
+//       description,
+//     });
 
-    // save the todo to the database
-    const savedTodo = await newTodo.save();
+//     // save the todo to the database
+//     const savedTodo = await newTodo.save();
 
-    // send the saved todo as a response
-    res.send({ message: "Todo created successfully", todo: savedTodo });
-  } catch (error) {
-    res.status(500).send({ message: error.message });
-  }
-});
+//     // send the saved todo as a response
+//     res.send({ message: "Todo created successfully", todo: savedTodo });
+//   } catch (error) {
+//     res.status(500).send({ message: error.message });
+//   }
+// });
 
 module.exports = app;
